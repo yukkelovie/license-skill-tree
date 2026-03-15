9cffa7e02141af0db737ab671e0dd6f31a579632
yukkelovie <yukke.lovie@gmail.com>
2026-03-15 12:13:54 +0900

    sw.js を作成

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = ‘app-v1’;
+const STATIC = [’/’];
+
+self.addEventListener(‘install’, e => {
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC))
+);
+self.skipWaiting();
+});
+
+self.addEventListener(‘activate’, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+)
+);
+self.clients.claim();
+});
+
+self.addEventListener(‘fetch’, e => {
+// Supabaseなど外部APIはキャッシュしない
+if(e.request.url.includes(‘supabase.co’) || e.request.url.includes(‘googleapis’)) {
+return;
+}
+e.respondWith(
+fetch(e.request)
+.then(res => {
+const clone = res.clone();
+caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+return res;
+})
+.catch(() => caches.match(e.request))
+);
+});
